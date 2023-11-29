083f2dabadaa8e28a4b88bf6789e8c9b3a1a523c
MXRQUEZ <svistunss.py@gmail.com>
2023-11-29 19:11:55 +0100

    better types

--- webpack/builders/plugin.ts
+++ webpack/builders/plugin.ts
@@ -7,13 +7,14 @@ import { ProcessEnvironmentVariables } from '../types/env';
 const createProcessEnvVariablesPlugin = (variables: ProcessEnvironmentVariables) => {
   type Keys = keyof typeof variables;
   type Values = (typeof variables)[Keys];
+  type Vars = Record<`__${Uppercase<Keys>}__`, Values>;
 
-  const vars: Record<string, Values> = Object.keys(variables).reduce(
+  const vars: Vars = Object.keys(variables).reduce(
     (accumulator, currentKey) => ({
       ...accumulator,
       [`__${currentKey.toUpperCase()}__`]: JSON.stringify(variables[currentKey as Keys]),
     }),
-    {},
+    {} as Vars,
   );
 
   return new DefinePlugin(vars);
